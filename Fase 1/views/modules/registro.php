3f8e5e0e4be62b20aa29bd19eb85a10d7e5bda7a
Gaby97 <1530108@upv.edu.mx>
2018-05-31 09:47:42 -0500

    Practica12

---
+++ Fase 1/views/modules/registro.php
@@ -0,0 +1,52 @@
+<h1>REGISTRO DE USUARIO</h1>
+
+      <form class="form-horizontal">
+                <div class="card-body">
+                  <div class="form-group">
+                    <label for="inputEmail3" class="col-sm-2 control-label">Email</label>
+
+                    <div class="col-sm-10">
+                      <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
+                    </div>
+                  </div>
+                  <div class="form-group">
+                    <label for="inputPassword3" class="col-sm-2 control-label">Password</label>
+
+                    <div class="col-sm-10">
+                      <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
+                    </div>
+                  </div>
+                  <div class="form-group">
+                    <div class="col-sm-offset-2 col-sm-10">
+                      <div class="form-check">
+                        <input type="checkbox" class="form-check-input" id="exampleCheck2">
+                        <label class="form-check-label" for="exampleCheck2">Remember me</label>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <!-- /.card-body -->
+                <div class="card-footer">
+                  <button type="submit" class="btn btn-info">Sign in</button>
+                  <button type="submit" class="btn btn-default float-right">Cancel</button>
+                </div>
+                <!-- /.card-footer -->
+              </form>
+
+<?php
+//Enviar los datos al controlador MvcController (es la clase principal de controller.php)
+$registro = new MvcController();
+//se invoca la función registroUsuarioController de la clase MvcController:
+$registro -> registroUsuarioController();
+
+if(isset($_GET["action"])){
+
+	if($_GET["action"] == "ok"){
+
+		echo "Registro Exitoso";
+	
+	}
+
+}
+
+?>
---
+++ Fase 1/views/template.php
@@ -0,0 +1,50 @@
+<!--Es la plantilla que vera el usuario al ejecutar la aplicación -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Template</title>
+
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+  <!-- Ionicons -->
+  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="../../dist/css/adminlte.min.css">
+  <!-- Google Font: Source Sans Pro -->
+  <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+  <style>
+
+		
+  </style>
+
+</head>
+
+<body>
+
+<?php include "modules/navegacion.php"; ?>
+
+
+<section>
+
+<?php 
+
+$mvc = new MvcController();
+$mvc -> enlacesPaginasController();
+
+ ?>
+
+</section>
+!-- jQuery -->
+<script src="../../plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="../../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- FastClick -->
+<script src="../../plugins/fastclick/fastclick.js"></script>
+<!-- AdminLTE App -->
+<script src="../../dist/js/adminlte.min.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="../../dist/js/demo.js"></script>
+	
+</body>
+
+</html>
